3bd329f0a04bc79958f9eeb44e0cb03f46a8e07b
hreypar <hreypar@gmail.com>
2020-02-04 21:42:34 -0600

    create script to generate human genes R object

---
+++ bin/obtain-human-genes.R
@@ -0,0 +1,13 @@
+###################################################################
+# hreyes february 2020
+#
+# obtain-human-genes.R
+#
+# create a granges object with the human genes
+# 
+###################################################################
+#################### import libraries and set options ####################
+
+library(annotables)
+library(magrittr)
+library(dplyr)
