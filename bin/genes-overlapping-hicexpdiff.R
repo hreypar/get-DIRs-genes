43a1bd61aa70366f87670f22fa955cf25ba523f7
hreypar <hreypar@gmail.com>
2020-02-04 21:47:25 -0600

    migrate gene object creation to the proper script

--- bin/genes-overlapping-hicexpdiff.R
+++ bin/genes-overlapping-hicexpdiff.R
@@ -6,8 +6,7 @@
 # read in multihiccompare normalized data
 # 
 ###################################################################
-#setwd("/home/hreyes/Desktop/february20/genes-analysis-40kb/")
-
+#setwd("~/Desktop/february20/get-diff-genes/")
 #################### import libraries and set options ####################
 library(magrittr)
 library(dplyr)
@@ -28,31 +27,6 @@ options(scipen = 10)
 # library(DOSE)
 # library(ROntoTools)
 # library(graph)
-
-####################obtain GRch38 gene symbols ###########################
-# THIS SHOULD BE A DIFFERENT SCRIPT IN BIN
-#
-# get protein coding genes for chr 1 to 22 and X
-# FIGURE OUT IF I ACTUALLY ONLY WANT PROTEIN CODING GENES... 
-# I MIGHT BE INTERESTED IN ncRNAs AND/OR PSEUDOGENES
-#
-grch38 %>%
-  subset(., biotype == "protein_coding") %>%
-  subset(., chr %in% c(1:22, "X")) -> grch38_symbols
-
-# transform X chr to make it compatible with hicexp format
-grch38_symbols$chr[grch38_symbols$chr == "X"] <- 23
-grch38_symbols$chr <- paste0("chr", grch38_symbols$chr)
-
-grch38_symbols$strand <- ifelse(grch38_symbols$strand == -1, "-", "+")
-
-# all genes granges
-grch38_symbols.gr <- makeGRangesFromDataFrame(grch38_symbols,
-                                              seqnames.field = "chr",
-                                              start.field = "start",
-                                              end.field = "end",
-                                              strand.field = "strand",
-                                              keep.extra.columns = TRUE)
 
 ########################## read in data ###################################
 # load("data_hicexp_objects_cyclic_loess_glm/NC_cycnorm_glm.Rdata")
--- bin/obtain-human-genes.R
+++ bin/obtain-human-genes.R
@@ -6,8 +6,34 @@
 # create a granges object with the human genes
 # 
 ###################################################################
+#setwd("~/Desktop/february20/get-diff-genes/")
 #################### import libraries and set options ####################
-
 library(annotables)
 library(magrittr)
 library(dplyr)
+#
+#
+####################obtain GRch38 gene symbols ###########################
+# THIS SHOULD BE A DIFFERENT SCRIPT IN BIN
+#
+# get protein coding genes for chr 1 to 22 and X
+# FIGURE OUT IF I ACTUALLY ONLY WANT PROTEIN CODING GENES... 
+# I MIGHT BE INTERESTED IN ncRNAs AND/OR PSEUDOGENES
+#
+grch38 %>%
+  subset(., biotype == "protein_coding") %>%
+  subset(., chr %in% c(1:22, "X")) -> grch38_symbols
+
+# transform X chr to make it compatible with hicexp format
+grch38_symbols$chr[grch38_symbols$chr == "X"] <- 23
+grch38_symbols$chr <- paste0("chr", grch38_symbols$chr)
+
+grch38_symbols$strand <- ifelse(grch38_symbols$strand == -1, "-", "+")
+
+# all genes granges
+grch38_symbols.gr <- makeGRangesFromDataFrame(grch38_symbols,
+                                              seqnames.field = "chr",
+                                              start.field = "start",
+                                              end.field = "end",
+                                              strand.field = "strand",
+                                              keep.extra.columns = TRUE)
