6c9197f254a1da3c852b6827c5dc8e0e85a182de
hreypar <hreypar@gmail.com>
2020-06-29 22:11:36 -0500

    add variable to select any number of biotypes

--- bin/obtain-human-genes.R
+++ bin/obtain-human-genes.R
@@ -1,39 +1,38 @@
-###################################################################
-# hreyes february 2020
+#!/usr/bin/env Rscript
 #
+# hreyes June 2020
 # obtain-human-genes.R
-#
-# create a granges object with the human genes
-# 
 ###################################################################
-#setwd("~/Desktop/february20/get-diff-genes/")
-#################### import libraries and set options ####################
+# create a granges object with the human genes
+###################################################################
+#
+#################### import libraries and set options #############
 library(annotables)
-library(magrittr)
-library(dplyr)
+suppressMessages(library(dplyr))
 #
 #
-####################obtain GRch38 gene symbols ###########################
-# THIS SHOULD BE A DIFFERENT SCRIPT IN BIN
-#
-# get protein coding genes for chr 1 to 22 and X
-# FIGURE OUT IF I ACTUALLY ONLY WANT PROTEIN CODING GENES... 
-# I MIGHT BE INTERESTED IN ncRNAs AND/OR PSEUDOGENES
-#
+###################### obtain gene symbols ########################
+#my.biotypes = c("protein_coding", "miRNA")
+my.biotypes = c("protein_coding")
+
+
+# get protein coding genes for chromosomes 1 to 22 and X
 grch38 %>%
-  subset(., biotype == "protein_coding") %>%
-  subset(., chr %in% c(1:22, "X")) -> grch38_symbols
+  subset(., biotype %in% my.biotypes) %>%
+  subset(., chr %in% c(1:22, "X")) -> grch38.genes
 
-# transform X chr to make it compatible with hicexp format
-grch38_symbols$chr[grch38_symbols$chr == "X"] <- 23
-grch38_symbols$chr <- paste0("chr", grch38_symbols$chr)
+# remap chrX for compatibility with hicexp results
+grch38.genes$chr[grch38.genes$chr == "X"] <- 23
+grch38.genes$chr <- paste0("chr", grch38.genes$chr)
 
-grch38_symbols$strand <- ifelse(grch38_symbols$strand == -1, "-", "+")
+grch38.genes$strand <- ifelse(grch38.genes$strand == -1, "-", "+")
 
-# all genes granges
-grch38_symbols.gr <- makeGRangesFromDataFrame(grch38_symbols,
+###################### convert to granges #########################
+grch38.genes.gr <- makeGRangesFromDataFrame(grch38.genes,
                                               seqnames.field = "chr",
                                               start.field = "start",
                                               end.field = "end",
                                               strand.field = "strand",
                                               keep.extra.columns = TRUE)
+
+#rm(my.biotypes, grch38.genes)
